b054fbf703aac300172202942e770af549855c58
Chandrakana Nandi <chandra.nandi90@gmail.com>
2024-10-15 19:46:49 -0700

    updates

--- src/certora/mod.rs
+++ src/certora/mod.rs
@@ -1,2 +1,2 @@
 pub mod token;
-pub mod spec;
+pub mod specs1;
--- src/types/contract_config.rs
+++ src/types/contract_config.rs
@@ -1,4 +1,5 @@
 use soroban_sdk::{contracttype, Address};
+use nondet::Nondet;
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -11,4 +12,14 @@ pub struct ContractConfig {
     pub token: Address,
     // Base contract fee amount
     pub fee: u64
+}
+
+impl Nondet for ContractConfig {
+    fn nondet() -> Self {
+        return ContractConfig {
+            admin: Address::nondet(),
+            token: Address::nondet(),
+            fee: u64::nondet()
+        };
+    }
 }
